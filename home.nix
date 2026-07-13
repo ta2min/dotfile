0178483c07d90dde58309373017befb34fbe4cae
ta2min <ta2mi.prostudy@gmail.com>
2026-07-13 11:14:56 +0900

    update

--- home.nix
+++ home.nix
@@ -29,7 +29,7 @@
   };
 
   home.sessionVariables = {
-    SSH_AUTH_SOCK = "''$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    SSH_AUTH_SOCK = "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
   };
 
   # Home Manager自体にプログラム管理を任せる
